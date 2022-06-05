28f7669caffe6b0fe8d07fa8e8fecd30f6ffbfd4
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-06-05 10:49:00 -0600

    Area del triangulo + area de circulo python

---
+++ tareas-python/area-triangulo.py
@@ -0,0 +1,20 @@
+
+base = 5
+altura = 10
+
+def triangulo(base, altura):
+     areaT = (base * altura) / 2
+     return areaT
+...
+triangulo(20, 30)
+
+## area de circulo
+
+radio = 5
+PI = 3.1416
+
+def circulo(radio):
+   areaa = PI * radio **2
+   return areaa
+
+circulo(10)
