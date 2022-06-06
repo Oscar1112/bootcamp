68abf545d2689643690ee4e6a0c3778df1a2acf7
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-06-05 19:49:12 -0600

    Update clases-tarea1.py

--- tareas-python/clases-tarea1.py
+++ tareas-python/clases-tarea1.py
@@ -1,14 +1,1 @@
-from contextlib import redirect_stderr
 
-
-class Vehiculo:
-    color = "red",
-    ruedas = 4,
-    puertas = 5
-
-class Coche(Vehiculo):
-    Velocidad = 200
-    cilindrada = 8
-
-C = Coche()
-print(C)
