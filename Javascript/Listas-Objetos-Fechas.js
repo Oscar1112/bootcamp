3dc8f42d2d1834df4100b61146087d4dcf90247f
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-12 14:56:40 -0600

    Add files via upload

---
+++ Javascript/Listas-Objetos-Fechas.js
@@ -0,0 +1,17 @@
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+lista = ["Oscar", 21, true, new Date("December 11 2001"),  {
+    titulo: "habitos atomicos",
+    autor: "James Clear",
+    fecha: new Date("Septiembre 8 2020"),
+    url: "https://www.amazon.com/-/es/James-Clear/dp/8418118032",
+}]
+console.log(lista)
