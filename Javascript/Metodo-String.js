1efc113b7cd344756a6f012d48f53da9f26ef2e1
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-13 15:38:25 -0600

    Add files via upload

---
+++ Javascript/Metodo-String.js
@@ -0,0 +1,29 @@
+let nombre = "Oscar";
+let apellido = " Flores";
+
+let estudiante =(nombre.concat(apellido));
+console.log(estudiante)
+
+
+
+let estudianteMayus = (estudiante.toUpperCase());
+console.log(estudianteMayus);
+
+let estudianteMinus = (estudiante.toLowerCase());
+console.log(estudianteMinus)
+
+longitud = estudiante.length;
+console.log(longitud)
+
+let Primera_Letra = (estudiante.charAt(1))
+console.log(nombre.charAt(0));
+
+last_letter = (apellido.charAt(apellido.length -1))
+console.log(last_letter)
+
+sinEspacio = (estudiante.replace(" ", ""))
+console.log(sinEspacio)
+
+verificacion = (estudiante.includes("Oscar"))
+console.log(verificacion)
+
