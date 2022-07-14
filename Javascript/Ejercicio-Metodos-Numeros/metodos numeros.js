7ae7e4fab7616a3e9afca33ba824ef3c95541202
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-14 10:13:44 -0600

    Add files via upload

---
+++ Javascript/Ejercicio-Metodos-Numeros/metodos numeros.js
@@ -0,0 +1,13 @@
+let altura_cm = 170
+let altura_mts = 1.70
+let peso_kg = 60.36
+//Altura redondeada hacia arriba
+let redondeo1 = Math.ceil(altura_mts)
+console.log(redondeo1)
+
+//redondeo hacia abajo
+let redondeo2 = Math.floor(peso_kg)
+console.log(redondeo2)
+
+num_max = (Number.MAX_VALUE + 1) == (Number.MAX_VALUE)
+console.log(num_max)
