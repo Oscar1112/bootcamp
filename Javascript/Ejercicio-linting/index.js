d48b876e73aa5beaefd080c38274cd65f407bf65
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-20 20:39:45 -0600

    Add files via upload

---
+++ Javascript/Ejercicio-linting/index.js
@@ -0,0 +1,32 @@
+let nombre = "Oscar"
+let apellido = " Flores"
+
+let estudiante =(nombre.concat(apellido))
+console.log(estudiante)
+
+
+
+let estudianteMayus = (estudiante.toUpperCase())
+console.log(estudianteMayus)
+
+let estudianteMinus = (estudiante.toLowerCase())
+console.log(estudianteMinus)
+
+longitud = estudiante.length
+console.log(longitud)
+
+let Primera_Letra = (estudiante.charAt(1))
+console.log(nombre.charAt(0))
+
+last_letter = (apellido.charAt(apellido.length -1))
+console.log(last_letter)
+
+sinEspacio = (estudiante.replace(" ", ""))
+console.log(sinEspacio)
+
+verificacion = (estudiante.includes("Oscar"))
+console.log(verificacion)
+
+let nombre1 = "Oscar"
+let nombre2 = "Oswaldo"
+let nombre3 = "Roxi"
