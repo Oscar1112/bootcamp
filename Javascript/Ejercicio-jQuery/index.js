d1ca00f16a00a4756eabbbd91ecdaf7c9376ba92
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-24 12:23:08 -0600

    Add files via upload

---
+++ Javascript/Ejercicio-jQuery/index.js
@@ -0,0 +1,8 @@
+const alerta = document.querySelector("button");
+alerta.addEventListener("click", () => {
+alert("click en el boton")
+})
+
+$(".Botoni").click(function(){
+    console.log("Hola estoy usando jQuery")
+})
