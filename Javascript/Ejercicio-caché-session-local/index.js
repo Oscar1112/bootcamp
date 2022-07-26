390e7509b184c6bd9931cd6729c6566b864b641a
Oscar1112 <103161181+Oscar1112@users.noreply.github.com>
2022-07-25 18:10:21 -0600

    Add files via upload

---
+++ Javascript/Ejercicio-caché-session-local/index.js
@@ -0,0 +1,12 @@
+const nombre = "Oscar";
+const apellido = "Flores";
+
+const info_personal = {nombre: "Oscar",
+apellido: "Flores"}
+
+// sessionStorage.setItem("perfil", JSON.stringify(info_personal))
+// localStorage.setItem("perfil1", JSON.stringify(info_personal))
+
+const fecha = new Date()
+// document.cookie = `perfil3=${JSON.stringify(info_personal)};expires=${new Date(fecha.getTime() + 2 * 60000)}`
+
